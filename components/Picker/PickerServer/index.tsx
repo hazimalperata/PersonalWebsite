8477926085880c5af694f3b10cec399d01d919c4
Hazim Alper Ata <hazimata@std.iyte.edu.tr>
2024-10-24 21:48:45 +0300

    Deneme

--- components/Picker/PickerServer/index.tsx
+++
@@ -1,8 +0,0 @@
-'use server'
-
-import {promises as fs} from 'fs';
-
-export async function getFile() {
-  const file = await fs.readFile(process.cwd() + '/public/accounts.txt', 'utf8');
-  return file;
-}
--- components/Picker/index.tsx
+++ components/Picker/index.tsx
@@ -1,23 +1,24 @@
 'use client'
 
-import {useCallback, useEffect, useRef, useState} from "react";
-import {getFile} from "@/components/Picker/PickerServer";
+import {useCallback, useRef, useState} from "react";
 
-export default function AccountPicker() {
-  const [accountsFile, setAccountsFile] = useState("");
+type Props = {
+  file: string;
+}
+
+export default function AccountPicker(props: Props) {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [isProcessing, setIsProcessing] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
 
   const spanRef = useRef<HTMLSpanElement>(null);
 
   const separator = "=========== List By TheFry =============="
 
   const onClickButton = useCallback(async () => {
-    if (isProcessing || isLoading) return;
+    if (isProcessing) return;
     setIsProcessing(true);
-    const accountList = accountsFile.split(separator);
+    const accountList = props.file.split(separator);
     if (spanRef.current) {
       const randomAccountText = accountList[Math.floor(Math.random() * accountList.length)];
       spanRef.current.innerText = randomAccountText;
@@ -28,35 +29,19 @@ export default function AccountPicker() {
       setPassword(passwordText.split(" = ")[1]);
     }
     setIsProcessing(false);
-  }, [accountsFile, isLoading, isProcessing]);
-
-  useEffect(() => {
-    (async () => {
-      const file = await getFile();
-      setAccountsFile(file);
-      setIsLoading(false);
-    })()
-  }, []);
+  }, [isProcessing, props.file]);
 
   return (
     <div className="flex flex-col gap-y-5 bg-slate-700 rounded-xl p-8 shadow-2xl max-w-screen-2xl">
-      {isLoading ? (
-        <div>
-          YÜKLENİYOR...
-        </div>
-      ) : (
-        <>
-          <button disabled={isProcessing} onClick={onClickButton} className="py-2 px-4 bg-slate-500 rounded-full hover:scale-105 transition-all duration-200">
-            Hesap al
-          </button>
-          <div className="flex flex-col gap-y-2">
-            <div className="cursor-pointer" onClick={() => navigator.clipboard.writeText(userName)}>Kullanıcı Adı: {userName}</div>
-            <div className="cursor-pointer" onClick={() => navigator.clipboard.writeText(password)}>Şifre: {password}</div>
-          </div>
-          <span ref={spanRef} className="bg-gray-400 rounded-lg py-2 px-4">
+      <button disabled={isProcessing} onClick={onClickButton} className="py-2 px-4 bg-slate-500 rounded-full hover:scale-105 transition-all duration-200">
+        Hesap al
+      </button>
+      <div className="flex flex-col gap-y-2">
+        <div className="cursor-pointer" onClick={() => navigator.clipboard.writeText(userName)}>Kullanıcı Adı: {userName}</div>
+        <div className="cursor-pointer" onClick={() => navigator.clipboard.writeText(password)}>Şifre: {password}</div>
+      </div>
+      <span ref={spanRef} className="bg-gray-400 rounded-lg py-2 px-4">
           </span>
-        </>
-      )}
     </div>
   )
 }
