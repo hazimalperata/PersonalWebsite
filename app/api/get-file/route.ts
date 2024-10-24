014cbcaffbf793e421a4267785f74b296f1ec4ac
Hazim Alper Ata <hazimata@std.iyte.edu.tr>
2024-10-24 22:07:16 +0300

    FIX random picker

--- app/api/get-file/route.ts
+++ app/api/get-file/route.ts
@@ -1,11 +1,19 @@
-import { NextResponse } from 'next/server';
-import { promises as fs } from 'fs';
+import {NextResponse} from 'next/server';
+import {promises as fs} from 'fs';
+
+// export type RandomAccount = {
+//
+// }
 
 export async function GET() {
+  const separator = "=========== List By TheFry =============="
+
   try {
     const file = await fs.readFile(process.cwd() + '/public/accounts.txt', 'utf8');
-    return NextResponse.json({ content: file });
+    const accountList = file.split(separator);
+    const randomAccount = accountList[Math.floor(Math.random() * accountList.length)];
+    return NextResponse.json({content: randomAccount});
   } catch (error) {
-    return NextResponse.json({ error: 'Dosya okunamadı' }, { status: 500 });
+    return NextResponse.json({error: 'Dosya okunamadı'}, {status: 500});
   }
 }
--- components/Picker/index.tsx
+++ components/Picker/index.tsx
@@ -3,65 +3,53 @@
 import {useCallback, useEffect, useRef, useState} from "react";
 
 export default function AccountPicker() {
-  const [accountsFile, setAccountsFile] = useState("");
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [isProcessing, setIsProcessing] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
 
   const spanRef = useRef<HTMLSpanElement>(null);
 
-  const separator = "=========== List By TheFry =============="
-
-  const onClickButton = useCallback(async () => {
+  const getRandomAccount = useCallback(async () => {
     if (isProcessing) return;
     setIsProcessing(true);
-    const accountList = accountsFile.split(separator);
+    const response = await fetch('/api/get-file');
+    const result = await response.json();
     if (spanRef.current) {
-      const randomAccountText = accountList[Math.floor(Math.random() * accountList.length)];
-      spanRef.current.innerText = randomAccountText;
-      const parsedList = randomAccountText.split("\r\n");
+      spanRef.current.innerText = result.content;
+      const parsedList = result.content.split("\r\n");
       const userNameText = parsedList[2];
       const passwordText = parsedList[3];
       setUserName(userNameText.split(" = ")[1]);
       setPassword(passwordText.split(" = ")[1]);
     }
     setIsProcessing(false);
-  }, [accountsFile, isProcessing]);
+  }, [isProcessing]);
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const response = await fetch('/api/get-file');
-        const data = await response.json();
-        setAccountsFile(data.content);
-        setIsLoading(false);
-      } catch (error) {
-        console.error("Dosya okunamadı:", error);
-        setIsLoading(false);
-      }
-    })()
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       const response = await fetch('/api/get-file');
+  //       const data = await response.json();
+  //       setAccountsFile(data.content);
+  //       setIsLoading(false);
+  //     } catch (error) {
+  //       console.error("Dosya okunamadı:", error);
+  //       setIsLoading(false);
+  //     }
+  //   })()
+  // }, []);
 
   return (
     <div className="flex flex-col gap-y-5 bg-slate-700 rounded-xl p-8 shadow-2xl max-w-screen-2xl">
-      {isLoading ? (
-        <div>
-          YÜKLENİYOR...
-        </div>
-      ) : (
-        <>
-          <button disabled={isProcessing} onClick={onClickButton} className="py-2 px-4 bg-slate-500 rounded-full hover:scale-105 transition-all duration-200">
-            Hesap al
-          </button>
-          <div className="flex flex-col gap-y-2">
-            <div className="cursor-pointer" onClick={() => navigator.clipboard.writeText(userName)}>Kullanıcı Adı: {userName}</div>
-            <div className="cursor-pointer" onClick={() => navigator.clipboard.writeText(password)}>Şifre: {password}</div>
-          </div>
-          <span ref={spanRef} className="bg-gray-400 rounded-lg py-2 px-4">
+      <button disabled={isProcessing} onClick={getRandomAccount} className="py-2 px-4 bg-slate-500 rounded-full hover:scale-105 transition-all duration-200">
+        Hesap al
+      </button>
+      <div className="flex flex-col gap-y-2">
+        <div className="cursor-pointer" onClick={() => navigator.clipboard.writeText(userName)}>Kullanıcı Adı: {userName}</div>
+        <div className="cursor-pointer" onClick={() => navigator.clipboard.writeText(password)}>Şifre: {password}</div>
+      </div>
+      <span ref={spanRef} className="bg-gray-400 rounded-lg py-2 px-4">
           </span>
-        </>
-      )}
     </div>
   )
 }
