f850c555c0014304544ae8eb8acbc5c4dcc69f5a
Hazim Alper Ata <hazimata@std.iyte.edu.tr>
2025-01-19 21:20:21 +0300

    TRY revalidate path

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -5,38 +5,47 @@ import { SanityContentTag } from "@/sanity/client";
 
 const secret = process.env.SANITY_REVALIDATE_SECRET;
 
+async function readBody(readable: any) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks).toString("utf8");
+}
+
 export async function GET(req: NextRequest) {
   const signature = req.headers.get(SIGNATURE_HEADER_NAME);
 
   console.error(signature);
 
-  revalidateTag(SanityContentTag);
-  console.error("Successfully Revalidated");
+  // revalidateTag(SanityContentTag);
+  // console.error("Successfully Revalidated");
 
-  return NextResponse.json("Revalidated", { status: 200 });
+  // return NextResponse.json("Revalidated", { status: 200 });
+  const body = await readBody(req);
+  console.error(body);
 
-  // try {
-  //   const data = await req.json();
-  //   if (!secret) {
-  //     console.log("Secret Problem");
-  //     return NextResponse.json("Secret Error", { status: 500 });
-  //   }
-  //
-  //   if (!signature) {
-  //     console.log("Signature Problem");
-  //     return NextResponse.json("No signature.", { status: 401 });
-  //   }
-  //
-  //   if (!(await isValidSignature(data, signature, secret))) {
-  //     console.log("Invalid Problem");
-  //     return NextResponse.json("Invalid signature.", { status: 401 });
-  //   }
-  //
-  //   revalidateTag(SanityContentTag);
-  //   console.log("Successfully Revalidated");
-  //
-  //   return NextResponse.json("Revalidated", { status: 200 });
-  // } catch (e) {
-  //   return NextResponse.json(`Secret Error ${e}`, { status: 500 });
-  // }
+  try {
+    if (!secret) {
+      console.log("Secret Problem");
+      return NextResponse.json("Secret Error", { status: 500 });
+    }
+
+    if (!signature) {
+      console.log("Signature Problem");
+      return NextResponse.json("No signature.", { status: 401 });
+    }
+
+    if (!(await isValidSignature(body, signature, secret))) {
+      console.log("Invalid Problem");
+      return NextResponse.json("Invalid signature.", { status: 401 });
+    }
+
+    revalidateTag(SanityContentTag);
+    console.log("Successfully Revalidated");
+
+    return NextResponse.json("Revalidated", { status: 200 });
+  } catch (e) {
+    return NextResponse.json(`Secret Error ${e}`, { status: 500 });
+  }
 }
