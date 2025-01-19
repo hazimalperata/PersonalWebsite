0ed64a96c61b2d3c4744f3a9b17e9ee5a7033048
Hazim Alper Ata <hazimata@std.iyte.edu.tr>
2025-01-19 20:14:20 +0300

    UPDATE route on revalidate

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -25,4 +25,6 @@ export async function POST(req: NextRequest) {
 
   revalidateTag(SanityContentTag);
   console.log("Successfully Revalidated");
+
+  return new Response("Revalidated", { status: 200 });
 }
