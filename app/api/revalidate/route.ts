4849ac6038311a1eda30f772eee926733f750ea8
Hazim Alper Ata <hazimata@std.iyte.edu.tr>
2025-01-21 19:51:23 +0300

    FIX security dependabot alerts

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -5,48 +5,25 @@ import { SanityContentTag } from "@/sanity/client";
 
 const secret = process.env.SANITY_REVALIDATE_SECRET;
 
-async function readBody(readable: any) {
-  const chunks = [];
-  for await (const chunk of readable) {
-    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
-  }
-  return Buffer.concat(chunks).toString("utf8");
-}
-
 export async function POST(req: NextRequest) {
   const signature = req.headers.get(SIGNATURE_HEADER_NAME);
 
-  console.error(signature);
-
-  // revalidateTag(SanityContentTag);
-  // console.error("Successfully Revalidated");
-  console.error(req);
-  console.error(req.body);
   const body = await req.text();
-  console.error(body);
-
-  // return NextResponse.json("Revalidated", { status: 200 });
-  // const body = await readBody(req);
-  // console.error(body);
 
   try {
     if (!secret) {
-      console.log("Secret Problem");
       return NextResponse.json("Secret Error", { status: 500 });
     }
 
     if (!signature) {
-      console.log("Signature Problem");
       return NextResponse.json("No signature.", { status: 401 });
     }
 
     if (!(await isValidSignature(body, signature, secret))) {
-      console.log("Invalid Problem");
       return NextResponse.json("Invalid signature.", { status: 401 });
     }
 
     revalidateTag(SanityContentTag);
-    console.log("Successfully Revalidated");
 
     return NextResponse.json("Revalidated", { status: 200 });
   } catch (e) {
